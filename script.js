eba0b66bf6729f03d966f89570df49ca0b46fe74
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-09-29 19:12:25 +0330

    indented with spaces

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 setInterval(() => {
-	const currentTime = new Date();
-	let hours = currentTime.getHours();
-	let minutes = currentTime.getMinutes();
-	let seconds = currentTime.getSeconds();
+  const currentTime = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
 
-	const period = hours >= 12 ? "PM" : "AM";
-	if (hours > 12) hours -= 12;
-	if (minutes < 10) minutes = "0" + minutes;
-	if (seconds < 10) seconds = "0" + seconds;
+  const period = hours >= 12 ? "PM" : "AM";
+  if (hours > 12) hours -= 12;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
 
-	const clockTime = `${hours}:${minutes}:${seconds} ${period}`;
-	document.getElementById("clock").innerText = clockTime;
+  const clockTime = `${hours}:${minutes}:${seconds} ${period}`;
+  document.getElementById("clock").innerText = clockTime;
 }, 1000);
